f9715e2461c63f5964854b5d7c68293c139794f6
rirefat <rirefat.official@gmail.com>
2024-03-02 00:56:09 +0600

    update modalBox

--- src/Components/ModalBox.jsx
+++ src/Components/ModalBox.jsx
@@ -1,6 +1,10 @@
-const ModalBox = ({ onClose,title }) => {
+const ModalBox = ({ onClose, title }) => {
     return (
-        <div className="opacity-90 absolute top-6 left-1/2 -translate-x-1/2 border w-fit px-8 py-3 flex flex-col justify-center items-center gap-8 shadow-lg rounded-md bg-slate-50">
+        <div
+            role="modal"
+            aria-modal='true'
+            className="opacity-90 absolute top-6 left-1/2 -translate-x-1/2 border w-fit px-8 py-3 flex flex-col justify-center items-center gap-8 shadow-lg rounded-md bg-slate-50"
+        >
             <h2 className="text-xl text-center">{title}</h2>
             <p className="w-48 text-center" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias impedit laudantium error corporis eius tenetur</p>
 
@@ -8,7 +12,7 @@ const ModalBox = ({ onClose,title }) => {
                 className="bg-red-200 px-4 py-1 rounded-lg text-red-900 font-semibold"
                 onClick={onClose}
             >
-                Close Modal
+                Close Modal ❌
             </button>
         </div>
     );
